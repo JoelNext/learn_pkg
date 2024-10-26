523d62f9603f5f482256e1b12ffbf2c0942feecf
JoelNext <18937989194@163.com>
2024-10-26 13:58:46 +0800

    main

--- learn_pkg/src/chao_node.cpp
+++ learn_pkg/src/chao_node.cpp
@@ -1,9 +1,23 @@
 #include <ros/ros.h>
+#include<std_msgs/String.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    printf("hello,world\n");
-    printf("nice,time 3.03 ");
-    printf("此时此刻已经很晚了，但我终于明白github怎么使用了！\n");
+    ros::init(argc,argv,"chao_node");
+    printf("我的枪去而复返，你的生命有去无回!\n ");
+
+    ros::NodeHandle n;
+    ros::Publisher pub=n.advertise<std_msgs::String>("kuai_shang_che_kai_hei_qun",20);
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+        printf("我要开始刷怪了\n");
+        std_msgs::String msg;
+        msg.data = "国服马超，带飞，上车走起！\n";
+        pub.publish(msg);
+        loop_rate.sleep();
+    }
+    
     return 0;
 }
--- turtle_vel_ctrl_pkg/src/turtle_vel_ctrl_node.cpp
+++ turtle_vel_ctrl_pkg/src/turtle_vel_ctrl_node.cpp
@@ -6,10 +6,17 @@ int main(int argc, char *argv[])
 {
     ros::init(argc,argv,"turtle_vel_ctrl_node");
     ros::NodeHandle n;
-     ros::Publisher vel_pub=n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",20);
+    ros::Publisher vel_pub=n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",20);
      while (ros::ok())
      {
         printf("小乌龟要开始移动了\n");
+        geometry_msgs::Twist vel_cmd;
+        vel_cmd.linear.x = 0.1;
+        vel_cmd.linear.y = 0;
+        vel_cmd.linear.z = 0;
+        vel_pub.publish(vel_cmd);
+        ros::spinOnce();
+
      }
     
 }
